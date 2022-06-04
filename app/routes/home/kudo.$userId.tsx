7c0eec7639ab6a41902df266fe255c94040d6e07
Mahilson Pereira <mahilson.pereira@acct.global>
2022-06-03 22:41:39 -0300

    layout kudo modal is done

--- app/routes/home/kudo.$userId.tsx
+++ app/routes/home/kudo.$userId.tsx
@@ -6,6 +6,10 @@ import { useState } from "react";
 import { Modal } from "~/components/modal";
 import { UserCircle } from "~/components/user-circle";
 import { getUserById } from "~/utils/users.server";
+import { SelectBox } from "~/components/select-box";
+import { colorMap, emojiMap } from "~/utils/constants";
+import { Kudo } from "~/components/kudo";
+import { getUser } from "~/utils/auth.server";
 
 export const loader: LoaderFunction = async ({ request, params }) => {
     const { userId } = params;
@@ -15,7 +19,9 @@ export const loader: LoaderFunction = async ({ request, params }) => {
     }
 
     const recipient = await getUserById(userId)
-    return json({ recipient })
+    const user = await getUser(request)
+
+    return json({ recipient, user })
 }
 
 export default function KudoModal() {
@@ -31,12 +37,32 @@ export default function KudoModal() {
     })
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>, field: string) => {
-        setFormData((data: any) => ({ ...data, [field]: e.target.value }))
+        setFormData(data => ({ ...data, [field]: e.target.value }))
     }
+
+    const handleStyleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>, field: string) => {
+        setFormData((data: any) => ({
+            ...data, style: {
+                ...data.style,
+                [field]: e.target.value
+            }
+        }))
+    }
+
+    const getOptions = (data: any) => Object.keys(data).reduce((acc: any[], curr) => {
+        acc.push({
+            name: curr.charAt(0).toUpperCase() + curr.slice(1).toLowerCase(),
+            value: curr
+        })
+        return acc
+    }, [])
+
+    const colors = getOptions(colorMap)
+    const emojis = getOptions(emojiMap)
 
     const {
         recipient,
-        /* user */
+        user,
     } = useLoaderData()
 
     return (
@@ -65,14 +91,43 @@ export default function KudoModal() {
                             placeholder={`Say something nice about ${recipient.profile.firstName}...`}
                         />
                         <div className="flex flex-col items-center md:flex-row md:justify-start gap-x-4">
-                            {/* Select Boxes Go Here */}
+                            <SelectBox
+                                options={colors}
+                                name="backgroundColor"
+                                value={formData.style.backgroundColor}
+                                onChange={e => handleStyleChange(e, 'backgroundColor')}
+                                label="Background Color"
+                                containerClassName="w-36"
+                                className="w-full rounded-xl px-3 py-2 text-gray-400"
+                            />
+                            <SelectBox
+                                options={colors}
+                                name="textColor"
+                                value={formData.style.textColor}
+                                onChange={e => handleStyleChange(e, 'textColor')}
+                                label="Text Color"
+                                containerClassName="w-36"
+                                className="w-full rounded-xl px-3 py-2 text-gray-400"
+                            />
+                            <SelectBox
+                                options={emojis}
+                                label="Emoji"
+                                name="emoji"
+                                value={formData.style.emoji}
+                                onChange={e => handleStyleChange(e, 'emoji')}
+                                containerClassName="w-36"
+                                className="w-full rounded-xl px-3 py-2 text-gray-400"
+                            />
                         </div>
                     </div>
                 </div>
                 <br />
                 <p className="text-blue-600 font-semibold mb-2">Preview</p>
                 <div className="flex flex-col items-center md:flex-row gap-x-24 gap-y-2 md:gap-y-0">
-                    {/* The Preview Goes Here */}
+                    <Kudo
+                        profile={user.profile}
+                        kudo={formData}
+                    />
                     <div className="flex-1" />
                     <button
                         type="submit"
